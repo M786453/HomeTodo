6180189e012aa6cd9b0bbda9f44ca7956182ec58
Ahtesham Sarwar <57073399+M786453@users.noreply.github.com>
2024-03-15 20:02:31 +0500

    Functionality for loading todos from db done.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,9 @@
+
+window.onload = function(){
+    load_todos()
+}
+
+
 function login(){
     
     var password = document.getElementById('password').value
@@ -24,7 +30,7 @@ function add_todo(){
 
     var cookie_data = document.cookie;
 
-    var home_password = cookie_data.split("=")[1]
+    var home_password = cookie_data.split("=")[1];
 
     if(user_input.trim().length > 0){
 
@@ -42,7 +48,9 @@ function add_todo(){
         xhr.onreadystatechange = function(){
             if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200){
 
-                console.log('Todo Saved.')
+                json_response = JSON.parse(xhr.responseText);
+
+                update_page(json_response);
 
             }
         }
@@ -54,6 +62,58 @@ function add_todo(){
     user_input_element.value = "";
 
     event.preventDefault();
+
+}
+
+function load_todos(){
+
+    var cookie_data = document.cookie;
+
+    var home_password = cookie_data.split("=")[1];
+
+    var xhr = new XMLHttpRequest();
+
+    var url = "http://127.0.0.1:5000/get_all_todos";
+
+    var params = "home_password=" + home_password;
+
+    xhr.open("POST", url, true);
+
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200){
+
+            json_response = JSON.parse(xhr.responseText);
+
+            update_page(json_response);
+
+        }
+    }
+
+    xhr.send(params)
+
+}
+
+function update_page(json_response){
+
+    for( var key in json_response){
+
+        var todo_text = json_response[key].todo;
+
+        var date = json_response[key].date;
+
+        var table = document.getElementById("todo-table");
+
+        var todo_row = table.insertRow();
+
+        todo_row.classList.add('todo-row');
+
+        var todo = todo_row.insertCell(0);
+        
+        todo.innerHTML += '<tr class="todo-row"><td><div class="todo"><div class="todo-header"><div class="date">Date: ' + date + '</div><div class="delete" onclick="delete_todo(this)"><i class="fas fa-trash"></i></div><div class="done" onclick="complete_todo(this)"><i class="fas fa-check"></i></div></div><div class="title">' + todo_text + '</div></div></td></tr>';
+
+    }
 
 }
 
